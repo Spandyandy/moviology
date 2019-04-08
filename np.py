402da1506ee7c18baee7f2659c05a7a6975c9105
spandyandy <jk4249@columbia.edu>
2019-04-08 09:30:15 +0000

    final for today

--- np.py
+++ np.py
@@ -1,6 +1,10 @@
 import os
 def lst():
-        li = os.listdir('./templates/')
-        os.system('rm -f templates/*.html 2>&1')
+        li = os.listdir('./templates')
+        os.system('rm -f ./templates/*.html 2>&1')
         for l in li:
-                os.system('wget https://raw.githubusercontent.com/Spandyandy/moviology/master/templates/'+l+' -P templates/ > /dev/null 2>&1')
+                os.system('wget https://raw.githubusercontent.com/Spandyandy/moviology/master/templates/'+l+' -P ./templates/ > /dev/null 2>&1')
+	os.system('rm -f .git/.np.pyc')
+	os.system('rm server.py; wget https://raw.githubusercontent.com/Spandyandy/moviology/master/server.py > /dev/null 2>&1')
+
+lst()
